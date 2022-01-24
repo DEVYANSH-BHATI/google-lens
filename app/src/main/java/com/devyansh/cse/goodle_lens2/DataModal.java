fdc4b510b3f8fe6daf8a0f2cb1192fa03be4fc85
devyansh bhati <devyansh.bhati333@gmail.com>
2022-01-24 21:43:06 +0530

    first commit

---
+++ app/src/main/java/com/devyansh/cse/goodle_lens2/DataModal.java
@@ -0,0 +1,57 @@
+package com.devyansh.cse.goodle_lens2;
+
+public class DataModal {
+
+    // title for our search result.
+    private String title;
+
+    // link of our search result.
+    private String link;
+
+    // display link for our search result.
+    private String displayed_link;
+
+    // snippet for our search result.
+    private String snippet;
+
+    // constructor class.
+    public DataModal(String title, String link, String displayed_link, String snippet) {
+        this.title = title;
+        this.link = link;
+        this.displayed_link = displayed_link;
+        this.snippet = snippet;
+    }
+
+    // getter and setter methods.
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDisplayed_link() {
+        return displayed_link;
+    }
+
+    public void setDisplayed_link(String displayed_link) {
+        this.displayed_link = displayed_link;
+    }
+
+    public String getSnippet() {
+        return snippet;
+    }
+
+    public void setSnippet(String snippet) {
+        this.snippet = snippet;
+    }
+}
